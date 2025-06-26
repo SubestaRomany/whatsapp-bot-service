508dfee7aa8a398c55655e7ce0f438289cc714dd
Subesta Romany <44113457-romanysubesta@users.noreply.replit.com>
2025-06-26 20:37:49 +0000

    update creds

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const { GoogleSpreadsheet } = require("google-spreadsheet");
-const creds = require("./creds.json");
+const creds = JSON.parse(process.env.CREDS_JSON);
 
 const app = express();
 app.use(express.json());
